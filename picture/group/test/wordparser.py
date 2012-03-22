af462b9322475e2d5762959a87fd41ef964c22e2
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-22 11:06:24 +0100

    Larger Exponents
    Multidigit exponents are supported now.

--- picture/group/test/wordparser.py
+++ picture/group/test/wordparser.py
@@ -10,6 +10,8 @@ class testWordParser(unittest.TestCase):
         self.assertEquals([('b', 1)], WordParser("b^1").factors())
         self.assertEquals([('a', 2)], WordParser("a^2").factors())
         self.assertEquals([('b', 2)], WordParser("b^2").factors())
+        self.assertEquals([('a', 10)], WordParser("a^10").factors())
+        self.assertEquals([('b', 10)], WordParser("b^10").factors())
         
     def testExceptionsThrown(self):
         self.assertRaises(ParseError, WordParser("^").factors)
--- picture/group/wordparser.py
+++ picture/group/wordparser.py
@@ -13,7 +13,9 @@ class WordParser:
             if (len(expression) > 1):
                 if (expression[1] == '^'):
                     index = 2
-                    number = int(expression[2])
+                    while (index < len(expression) and expression[index] in string.digits):
+                        index += 1
+                    number = int(expression[2:index])
                     return [(letter, number)]
             else:
               return [(letter, 1)]
