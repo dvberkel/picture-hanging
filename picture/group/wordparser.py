13b9ea2087b78b0382283a31f7506a8acfcac119
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-22 11:47:50 +0100

    [REFACTOR] Removed start
    the start variable is removed. Instead we relay in index aritmetic

--- picture/group/wordparser.py
+++ picture/group/wordparser.py
@@ -14,12 +14,11 @@ class WordParser:
             letter, number, index = expression[0], 1, 1
             if (len(expression) > 1):
                 if (len(expression) > 2 and expression[1] == '^'):
-                    start = 2
-                    index = (start + 1) if expression[start] == '-' else start
+                    index += 2 if expression[2] == '-' else 1
                     while (index < len(expression) and expression[index] in string.digits):
                         index += 1
                     try:
-                        number = int(expression[start:index])
+                        number = int(expression[2:index])
                     except ValueError:
                         raise ParseError()
             return [(letter, number)] + self._parse(expression[index:])
