3d3620d2b1055cb72887cafd7bf9578486e68249
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-22 12:03:43 +0100

    [REFACTOR] folded if
    Two ifs are folded into one.

--- picture/group/wordparser.py
+++ picture/group/wordparser.py
@@ -12,15 +12,14 @@ class WordParser:
             return []
         if (expression[0] in string.letters):
             letter, number, index = expression[0], 1, 1
-            if (len(expression) > 1):
-                if (len(expression) > 2 and expression[1] == '^'):
-                    index += 2 if expression[2] == '-' else 1
-                    while (index < len(expression) and expression[index] in string.digits):
-                        index += 1
-                    try:
-                        number = int(expression[2:index])
-                    except ValueError:
-                        raise ParseError()
+            if (len(expression) > 2 and expression[1] == '^'):
+                index += 2 if expression[2] == '-' else 1
+                while (index < len(expression) and expression[index] in string.digits):
+                    index += 1
+                try:
+                    number = int(expression[2:index])
+                except ValueError:
+                    raise ParseError()
             return [(letter, number)] + self._parse(expression[index:])
         raise ParseError()
 
