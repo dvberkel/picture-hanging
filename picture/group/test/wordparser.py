256a87ef2f25f2d39accd7cf4f355e5710ab32d6
Daan van Berkel <daan.v.berkel.1980@gmail.com>
2012-03-22 10:47:21 +0100

    Raising errros
    Errors are raised if the expression does not match expectations.

--- picture/group/test/wordparser.py
+++ picture/group/test/wordparser.py
@@ -1,6 +1,6 @@
 import unittest
 
-from picture.group.wordparser import WordParser
+from picture.group.wordparser import WordParser, ParseError
 
 class testWordParser(unittest.TestCase):
     def testWordParserFactors(self):
@@ -10,6 +10,9 @@ class testWordParser(unittest.TestCase):
         self.assertEquals([('b', 1)], WordParser("b^1").factors())
         self.assertEquals([('a', 2)], WordParser("a^2").factors())
         self.assertEquals([('b', 2)], WordParser("b^2").factors())
- 
+        
+    def testExceptionsThrown(self):
+        self.assertRaises(ParseError, WordParser("^").factors)
+
 if __name__ == '__main__':
     unittest.main()
--- picture/group/wordparser.py
+++ picture/group/wordparser.py
@@ -1,3 +1,5 @@
+import string
+
 class WordParser:
     def __init__(self, word):
         self.word = word
@@ -6,4 +8,10 @@ class WordParser:
         return self._parse(self.word)
 
     def _parse(self, expression):
-        return [(expression[0], int(expression[2]) if len(expression) > 1 else 1)]
+        if (expression[0] in string.letters):
+            return [(expression[0], int(expression[2]) if len(expression) > 1 else 1)]
+        raise ParseError()
+
+class ParseError:
+    pass
+        
